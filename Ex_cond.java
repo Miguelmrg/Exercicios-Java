3092ab95527a33101ee8f4d0581b7a6eb2dc8e59
Miguelmrg <141030876+Miguelmrg@users.noreply.github.com>
2025-01-08 20:54:42 -0300

    Add files via upload

---
+++ Ex_cond.java
@@ -0,0 +1,23 @@
+package defaultPackage;
+
+import java.util.Scanner;
+
+public class Ex_cond {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("digite um número inteiro: ");
+		
+		int n = sc.nextInt();
+		
+		if (n % 2 == 0) {
+			System.out.println("o número é par");
+		} else {
+			System.out.println("o número é ímpar");
+		}
+		sc.close();
+	}
+
+}
