ee33c44ad2427392bc5bc856d2a7812a7e55c35e
Miguelmrg <141030876+Miguelmrg@users.noreply.github.com>
2025-01-27 19:29:32 -0300

    Create Ex_Cond_6.java

---
+++ Ex_Cond_6.java
@@ -0,0 +1,36 @@
+package defaultPackage;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Ex_cond {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("digite um número ENTRE 0 e 100");
+		double num = sc.nextDouble();
+		
+		if (num >= 0.00 && num <= 25.00) {
+			System.out.printf("o número %.2f está no intervalo 0.00 e 25.00", num);
+		}
+		else if (num >= 25.01 && num <= 50.00) {
+			System.out.printf("o número %.2f está no intervalo 25.00 e 50.00", num);
+		}
+		else if (num >= 50.01 && num <= 75.00) {
+				System.out.printf("o número %.2f está no intervalo 50.00 e 75.00", num);
+		}
+		else if (num >= 75.01 && num <= 100.00) {
+			System.out.printf("o número %.2f está no intervalo 75.00 e 100.00", num);
+	}
+		else {
+			System.out.println("o número não está presente em nenhum intervalo do programa.");
+		}
+		
+		sc.close();
+	}
+
+}
