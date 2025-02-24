997bf170e52408966a4296f6e70fad427ecafab9
Miguelmrg <141030876+Miguelmrg@users.noreply.github.com>
2025-02-24 16:59:56 -0300

    Update Ex_Cond_4.java

--- Ex_Cond_4.java
+++ Ex_Cond_4.java
@@ -1,3 +1,7 @@
+/*Leia a hora inicial e a hora final de um jogo. A seguir calcule a duração do jogo, sabendo que o mesmo pode
+começar em um dia e terminar em outro, tendo uma duração mínima de 1 hora e máxima de 24 horas.*/
+
+
 package defaultPackage;
 
 import java.util.Scanner;
