6446a2d5d0b4c50bfc8ba1536c48e35a7f068128
Miguelmrg <141030876+Miguelmrg@users.noreply.github.com>
2025-02-24 15:49:41 -0300

    Create Ex_While_2.java

---
+++ Ex_While_2.java
@@ -0,0 +1,34 @@
+package defaultPackage;
+
+import java.util.Scanner;
+
+public class Enquanto {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int x = 1;
+		int y = 1;
+		
+		while (x != 0 || y != 0) {
+			System.out.println("Digite as coordenadas do ponto: (digite 0 , 0 para encerrar o programa)");
+			x = sc.nextInt();
+			y = sc.nextInt();
+			
+			if (x > 0 && y > 0) {
+				System.out.printf("o ponto ( %d , %d ) está no 1º quadrante. %n", x, y);
+			} 
+			else if (x < 0 && y > 0) {
+				System.out.printf("o ponto ( %d , %d ) está no 2º quadrante. %n", x, y);
+			}
+			else if (x < 0 && y < 0) {
+				System.out.printf("o ponto ( %d , %d ) está no 3º quadrante. %n", x, y);
+			}
+			else if (x > 0 && y < 0) {
+				System.out.printf("o ponto ( %d , %d ) está no 4º quadrante. %n", x, y);
+			}
+		}
+		sc.close();
+	}
+
+}
