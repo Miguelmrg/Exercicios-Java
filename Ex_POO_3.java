339b5795618973ede34b12c7e8a311f710bbb45d
Miguelmrg <141030876+Miguelmrg@users.noreply.github.com>
2025-03-17 14:14:57 -0300

    Update Ex_POO_3.java

--- Ex_POO_3.java
+++ Ex_POO_3.java
@@ -4,44 +4,72 @@ quantidade no estoque). Em seguida:
 estoque)
 • Realizar uma entrada no estoque e mostrar novamente os dados do produto
 • Realizar uma saída no estoque e mostrar novamente os dados do produto
-• UTILIZAR CONSTRUTOR */
-
+• UTILIZAR CONSTRUTOR E GETTERS/SETTERS*/
 package entidades;
 
 public class Produto {
-	
-	public String nome;
-	public double preço;
-	public int quantidade;
-	
+
+	private String nome;
+	private double preço;
+	private int quantidade;
+
+	public Produto() {
+	}
+
 	public Produto(String nome, double preço, int quantidade) {
+		super();
 		this.nome = nome;
 		this.preço = preço;
 		this.quantidade = quantidade;
 	}
-	
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public Produto(String nome, double preço) {
+		this.nome = nome;
+		this.preço = preço;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setPreço(double preço) {
+		this.preço = preço;
+	}
+
+	public double getPreço() {
+		return preço;
+	}
+
 	public double valorTotalEstoque() {
 		return preço * quantidade;
 	}
+
 	public void adicionarProdutos(int quantidade) {
 		this.quantidade += quantidade;
 	}
+
 	public void removerProdutos(int quantidade) {
 		this.quantidade -= quantidade;
 	}
+
 	public String toString() {
-		return nome
-		+", $"
-		+String.format("%.2f", preço)
-		+", "
-		+quantidade
-		+" unidades, Total: $ "
-		+String.format("%.2f",valorTotalEstoque());
-	
-				
+		return nome + ", $" + String.format("%.2f", preço) + ", " + quantidade + " unidades, Total: $ "
+				+ String.format("%.2f", valorTotalEstoque());
+
 	}
 }
-
 
 package aplicação;
 
@@ -68,6 +96,11 @@ public class ProgramaProduto {
 		produto.toString();
 
 		System.out.printf("%ndados do produto: " + produto + "%n");
+		
+		produto.setNome("Computador");
+		System.out.println("nome atualizado: "+produto.getNome());
+		produto.setPreço(1200);
+		System.out.println("preço atualizado: "+produto.getPreço());
 
 		System.out.printf("%ndigite o número de produtos a serem adicionados no estoque: ");
 		quantidade = sc.nextInt();
@@ -85,4 +118,3 @@ public class ProgramaProduto {
 	}
 
 }
-
