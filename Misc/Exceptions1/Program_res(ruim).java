1210e8ee79f482e07bf54f9e2944767f40eba923
Miguel Ribeiro Grillo <141030876+Miguelmrg@users.noreply.github.com>
2025-10-21 15:12:40 -0300

    Create Program_res(ruim).java

---
+++ Misc/Exceptions1/Program_res(ruim).java
@@ -0,0 +1,45 @@
+package application;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+import model.entities.Reservation;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        System.out.print("Room number: ");
+        int number = sc.nextInt();
+        System.out.print("Checkin date (dd/MM/yyyy): ");
+        LocalDate checkIn = LocalDate.parse(sc.next(), fmt);
+        System.out.print("Checkout date (dd/MM/yyyy): ");
+        LocalDate checkout = LocalDate.parse(sc.next(), fmt);
+
+        if (!checkout.isAfter(checkIn)) {
+            System.out.println("Error in reservation: checkout date must be after checkin date.");
+        } else {
+            Reservation reservation = new Reservation(number, checkIn, checkout);
+            System.out.println("Reservation: " + reservation);
+
+            System.out.println();
+            System.out.println("Enter data to update the reservation: ");
+            System.out.print("Checkin date (dd/MM/yyyy): ");
+            checkIn = LocalDate.parse(sc.next(), fmt);
+            System.out.print("Checkout date (dd/MM/yyyy): ");
+            checkout = LocalDate.parse(sc.next(), fmt);
+
+            String error = reservation.updateDates(checkIn, checkout);
+            if (error != null) {
+                System.out.println("Error in reservation: " + error);
+            } else {
+                System.out.println("Reservation: " + reservation);
+            }
+        }
+
+        sc.close();
+    }
+}
